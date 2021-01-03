e2eb1bd435db8aa8748bbb5e9800b06651b0ac4c
Alexandros Kontos <alexiskon01@gmail.com>
2021-01-03 15:34:23 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express()
 app.all('*', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader('Access-Control-Expose-Headers', 'Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'PATCH, GET, POST, DELETE, OPTIONS');
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
