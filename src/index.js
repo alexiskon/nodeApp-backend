f40a597178dde5a0c8ec5915df553583f6caf8f5
Alexandros Kontos <alexiskon01@gmail.com>
2021-01-03 16:02:59 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const app = express()
 app.all('*', function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://alexiskon.github.io/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://alexiskon.github.io/*');
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Authorization")
     res.setHeader('Access-Control-Allow-Methods', 'PATCH, GET, POST, DELETE, OPTIONS');
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
