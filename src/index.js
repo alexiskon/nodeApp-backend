31d5adb380f3f15fdf088a045b8df2d88756be36
Alexandros Kontos <alexiskon01@gmail.com>
2021-01-03 16:26:29 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const app = express()
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://alexiskon.github.io/');
+    res.header('Access-Control-Allow-Origin', 'https://alexiskon.github.io');
     res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
