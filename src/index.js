4d7c7c6c9e10e5bd750884f163b178a4a6a2fd9a
Alexandros Kontos <alexiskon01@gmail.com>
2021-01-03 15:16:15 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 
-require('./db/mongoose') //load in the database
-//load the models
-const User = require('./models/user')
-const tasks = require('./models/tasks')
-//
-
 const app = express()
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'PATCH, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
 
+require('./db/mongoose') //load in the database
+//load the models
+const User = require('./models/user')
+const tasks = require('./models/tasks')
+  
 app.use(express.json()) //auto parse incoming json to object
 
 //load the routes
